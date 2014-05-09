ad7f7e0266c96f7a1a14141c2ba12fc06e3325e5
dkeresteci <dkeresteci@gmail.com>
2014-05-09 22:58:29 +0000

    Enabled CORS in app.js

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/items', item.list);
